4e6bc5d22e6fc338e25393ba839d26a2fb56272d
TomLesky <dinhbinh599@gmail.com>
2020-03-28 18:31:20 +0700

    Update task fragment and layout

--- Work-Management-KhoiVD/AndroidCode/app/src/main/java/com/example/workmanager/constants/ResponseCodeConstant.java
+++
@@ -1,9 +0,0 @@
-package com.example.workmanager.constants;
-
-public class ResponseCodeConstant {
-    public static final int OK = 200;
-    public static final int ERROR = 500;
-    public static final int NOTFOUND = 404;
-    public static final int NOCONTENT = 204;
-    public static final int UNAUTHORIZED = 401;
-}
